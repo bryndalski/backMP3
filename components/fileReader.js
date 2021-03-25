d00fce15137c36b7530385a130222bcd0cdaeefd
bryndalski <kubabryndal@gmail.com>
2021-03-25 09:16:05 +0100

    back servera mp3 init

---
+++ components/fileReader.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const fs = require('fs')
+
+module.exports = async function returnFiles(directory, dirname) {
+    let fileRead = new Promise((suc, err) => {
+        new Promise((suc) => {
+            fs.readdir(path.join(dirname, directory), function (err, files) {
+                if (err) {
+                    return console.log(err);
+                }
+                new Promise((suc, er) => {
+                    let counter = 0
+                    let data = []
+                    let recursiveFunction = function () {
+                        data[files[counter]] = []
+                        fs.readdir(path.join(dirname, path.join(directory, files[counter])), async function (err, insidefiles) {
+                            if (err)
+                                return console.log(err);
+
+                            insidefiles.forEach(index => {
+                                console.log(index)
+                                let fileStats = fs.statSync(path.join(dirname, `${directory}${files[counter]}/${index}`))
+                                data.push({
+                                    source: `${directory}${files[counter]}/${index}`,
+                                    name: index,
+                                    size: Math.round((fileStats.size / (1024 * 1024) + Number.EPSILON) * 100) / 100,
+                                    path: files[counter]
+                                })
+                            })
+                            counter++
+                            if (counter === files.length)
+                                suc(data)
+                            else
+                                recursiveFunction()
+                        });
+                    }
+                    recursiveFunction()
+                }).then((val) => suc(val))
+            });
+        }).then(v => suc(v))
+    })
+    return fileRead
+}
---
+++ components/simpleExpress.js
@@ -0,0 +1,71 @@
+// const http = require('http')
+const fs = require('fs');
+const {
+    type
+} = require('os');
+require("colors");
+
+const contentType = {
+    html: "text/html",
+    plain: "text/plain",
+    css: "text/css",
+    json: "application/json",
+    xml: "text/xml",
+    mp4: "video/mp4",
+    jpeg: "image/jpeg",
+    png: "image/png",
+    js: "application/javascript",
+    mpeg: "audio/mpeg",
+    mp3: "audio / mpeg3",
+    ico: "image/ico",
+}
+
+
+
+module.exports = {
+    universalHeader: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        "Access-Control-Allow-Headers": "*"
+    },
+    extDetector: (url) => {
+        let extenction = url.split(".").pop()
+        if (extenction !== undefined)
+            return contentType[extenction]
+        else
+            return undefined
+
+    },
+    fileSend: (res, filePath, Ctype) => {
+        let sciezka = filePath
+        sciezka = filePath.replace(/%20/g, " ")
+        fs.readFile(sciezka, function (error, data) {
+            if (error) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.write("<h1>błąd 404 - nie ma pliku!<h1>");
+                res.end();
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': (Ctype == undefined) ? contentType.html : Ctype
+                });
+                res.write(data);
+                res.end();
+            }
+        });
+    },
+    sendJSON(res, data) {
+        console.log(data);
+        res.writeHead(200, {
+            'Content-Type': contentType.json,
+            'Accept': "application/xhtml+xml",
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials": true,
+            ...this.universalHeader
+        });
+        res.end(JSON.stringify(data));
+    }
+}
